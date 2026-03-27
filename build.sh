d7168b1726948b32d64ca56115a736786b0ceeb0
Dhvanil Bhagat <105095994+dhvanil3103@users.noreply.github.com>
2026-03-26 19:10:37 -0700

    fix(build): use apt-get

--- build.sh
+++ build.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
+
+echo "Installing system dependencies..."
+apt-get update && apt-get install -y ffmpeg
 
 echo "Installing Python dependencies..."
 pip install -r backend/requirements.txt
-
-echo "Downloading static ffmpeg..."
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-tar -xf ffmpeg-release-amd64-static.tar.xz
-
-echo "Setting up ffmpeg..."
-mkdir -p bin
-mv ffmpeg-*-amd64-static/ffmpeg bin/
-mv ffmpeg-*-amd64-static/ffprobe bin/
-
-echo "Cleaning up..."
-rm -rf ffmpeg-*-amd64-static*
 
 echo "Build complete!"
